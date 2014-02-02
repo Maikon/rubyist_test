16ac82926dbfa75553769d5fcd9037e6af6bc840
Makis Otman <makisotman@gmail.com>
2014-02-02 14:49:44 +0000

    fixed tests and finished method

--- task_1/lib/injecting.rb
+++ task_1/lib/injecting.rb
@@ -1,6 +1,10 @@
 class Array
 
   def injecting(*acc)
+    acc = acc.empty? ? self.shift : acc.first
+    self.each do |x|
+      acc = yield(acc,x)
+    end
     acc
   end
 
--- task_1/spec/injecting_spec.rb
+++ task_1/spec/injecting_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 describe 'injecting' do
-  let(:array) { Array.new }
-  
-  it 'array should have injecting method' do
-    expect(array.injecting).to be_true
+  let(:array) { [1,2,3] }
+
+  it 'should return the sum of the array' do
+    expect(array.injecting { |acc,val| acc + val }).to eq(6)
   end
 
-  it 'takes an optional argument' do
-    expect(array.injecting(1)).to be_true
+  it 'takes an argument and sets accumulator' do
+    expect(array.injecting(100) { |acc,val| acc + val }).to eq(106)
   end
+
+  it 'is able to use multiplication' do
+    expect(array.injecting(100) { |acc,val| acc * val }).to eq(600)
+  end
+
+  it 'returns longest word from array' do
+    array = ['hi','hello','wow']
+    expect(array.injecting { |first_w,next_w| first_w.length > next_w.length ? first_w : next_w }).to eq("hello")
+  end
+
 end

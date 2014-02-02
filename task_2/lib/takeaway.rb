da06a6daf48a2182d7d1409cf8765b3e75fcaec7
Makis Otman <makisotman@gmail.com>
2014-02-02 21:34:52 +0000

    added tests for menu and order

--- task_2/lib/takeaway.rb
+++ task_2/lib/takeaway.rb
@@ -1,3 +1,15 @@
 class Takeaway
+  attr_reader :menu, :order
+
+  MENU = {
+    pizza: 12.50,
+    dessert: 3.20,
+    drink: 2.99
+  }
+
+  def initialize
+    @menu = MENU
+    @order = []
+  end
 
 end
--- task_2/spec/takeaway_spec.rb
+++ task_2/spec/takeaway_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe Takeaway do
 
+let(:takeaway) { Takeaway.new }
+
+  it 'should have menu on initialization' do
+    expect(takeaway.menu).to eq({pizza: 12.50, dessert: 3.20, drink: 2.99 })
+  end
+
+  it 'should have an empty order on initialization' do
+    expect(takeaway.order).to eq([])
+  end
 end

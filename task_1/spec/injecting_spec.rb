df2ed82693e1ecf31a222cb404f3ff9b7c2fe384
Makis Otman <makisotman@gmail.com>
2014-02-02 17:07:32 +0000

    added one more test for building a hash

--- task_1/spec/injecting_spec.rb
+++ task_1/spec/injecting_spec.rb
@@ -4,20 +4,25 @@ describe 'injecting' do
   let(:array) { [1,2,3] }
 
   it 'should return the sum of the array' do
-    expect(array.injecting { |acc,val| acc + val }).to eq(6)
+    expect(array.injecting { |acc, val| acc + val }).to eq(6)
   end
 
   it 'takes an argument and sets accumulator' do
-    expect(array.injecting(100) { |acc,val| acc + val }).to eq(106)
+    expect(array.injecting(100) { |acc, val| acc + val }).to eq(106)
   end
 
   it 'is able to use multiplication' do
-    expect(array.injecting(100) { |acc,val| acc * val }).to eq(600)
+    expect(array.injecting(100) { |acc, val| acc * val }).to eq(600)
   end
 
   it 'returns longest word from array' do
     array = ['hi','hello','wow']
-    expect(array.injecting { |first_w,next_w| first_w.length > next_w.length ? first_w : next_w }).to eq("hello")
+    expect(array.injecting { |acc, val| acc.length > val.length ? acc : val }).to eq("hello")
+  end
+
+  it 'returns a hash from multi-dimensional array' do
+    array = [[:name,'joe'],[:last,'doe']]
+    expect(array.injecting({}) { |acc, val| acc[val.first] = val.last; acc }).to eq({:name => 'joe', :last => 'doe'})
   end
 
 end
